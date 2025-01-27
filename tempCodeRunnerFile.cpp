1e2ca7cc658ec4dd49f02d90ac32d22b55b1d16a
Thanaphon125 <suwanna153153@gmail.com>
2025-01-27 21:36:06 +0700

    complete

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+template <typename T>
+void insertionSort(T d[],int N){
+	for(int i=1; i<N; i++){
+		T tem   
+		int j = i-1;
+	}
+	while(j =0 && d[j]< key){
+		d[j+1] = d[j];
+		j--;
+		d[j+1] = key;
+	}
+	cout<<"pass "<<i <<":";
+	for (int k = 0; k<N; k++){
+		cout<< d[k] << ""<<endl;
+	}
+}
